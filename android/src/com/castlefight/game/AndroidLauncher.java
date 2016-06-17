414933156b0133c9143a033f0323e92711c0a871
sasha9012 <sasha901239@gmail.com>
2016-06-17 18:58:27 +0200

    Add files via upload

---
+++ android/src/com/castlefight/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.castlefight.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.castlefight.game.MainGameClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGameClass(), config);
+	}
+}
